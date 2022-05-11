f1b1e064ea7ee2bde057cc5f97c6317aac79c49b
Anthony <anthonygibson24@gmail.com>
2022-05-10 22:32:11 -0500

    successfully fetched data from Github API

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-var get1 = document.getElementById('button1');
-
-let test = document.getElementById('hello');
-
-const onclick = (this) => {
-    test.innerHTML = 'hello';
-    console.log(test.innerHTML)
-}
+var get1 = document.getElementById('name');
+var get2 = document.getElementById('following');
+var get3 = document.getElementById('numRepos');
+var get4 = document.getElementById('numFollowers');
 
 
 async function github() {
-    response = await fetch("https://api.github.com/users/adion81");
+    response = await fetch("https://api.github.com/users/anthonycg");
     coderData = await response.json();
+    get1.innerHTML = coderData.name;
+    get2.innerHTML = coderData.following;
+    get3.innerHTML = coderData.public_repos;
+    get4.innerHTML = coderData.followers;
+    console.log(coderData.name)
     return coderData;
     
 }
 
-get1.addEventListener('click', github);
+
+const followers = () => {
+    get4.style.backgroundColor = 'blue'
+}
+
+get4.addEventListener('mouseover', followers);
